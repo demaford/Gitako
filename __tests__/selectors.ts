25a296d2c05541f667938e25581f639d30b89de2
EnixCoda <enixcoda@gmail.com>
2025-01-16 23:10:15 +0800

    test: update selector

--- __tests__/selectors.ts
+++ __tests__/selectors.ts
@@ -2,7 +2,10 @@ export const selectors = {
   github: {
     breadcrumbFileName: `[data-testid="breadcrumbs-filename"]`,
     fileContent: 'textarea[aria-label="file content"]',
-    commitLinks: `li[data-testid="commit-row-item"] [data-testid="list-view-item-title-container"] a[href*="/commit/"]`,
+    commitLinks: [
+      `li[data-testid="commit-row-item"] [data-testid="list-view-item-title-container"] a[href*="/commit/"]`,
+      `li[data-testid="commit-row-item"] h4 a[href*="/commit/"]`,
+    ].join(),
     // assume title contains `.` is file item
     fileListItemFileLinks: `table[aria-labelledby="folders-and-files"] tr.react-directory-row td.react-directory-row-name-cell-large-screen .react-directory-filename-column .react-directory-truncate a[aria-label$="(File)"]`,
     fileListItemLinkOf: (name: string) =>
